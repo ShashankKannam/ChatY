a31decc5a96505a827bd05caa83a9de349a6ed88
IOS Course Project <ioscourseproject@MacBook-Pro.local>
2016-12-03 12:37:09 -0600

    Initial Commit

---
+++ ChatZ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatZ
+//
+//  Created by IOS Course Project on 12/3/16.
+//  Copyright © 2016 IOS Course Projectvb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
